248176b8ed89e6154eb16501e7f10ec06e4997cd
samea-jesus0 <sameasilva198@gmail.com>
2025-08-18 22:04:20 -0300

    Exercicio de cálculo da força

---
+++ funcao.kt
@@ -0,0 +1,16 @@
+// Calcular a Força
+
+fun main(){
+    println("Digite a massa (formula: F = m * a) : ")
+    var m = readln().toDouble() // lendo o valor da massa
+    println("Digite a aceleração: ")
+    var a = readln().toDouble() // lendo o valor da aceleração
+
+    println("A força resultante é: ${calculoForca(m, a)} N")  // chamando a função para calcular e quando o resultado voltar irá ser mostrado na tela.
+}
+
+fun calculoForca(m: Double, a: Double): Double { // a função está esperando dois argumentos Double e irá retornar em Double
+    var f = m * a // formula para calcular a força
+    return f // devolvendo o resultado
+}
+
